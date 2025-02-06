c734277b24b92dd4ff7e7a8f2d49ad685a2b4589
Steinar Gijze <steinar-gijze@hotmail.nl>
2025-02-06 14:13:29 +0100

    Read text file

--- BlockMaker/gui/main_window.py
+++ BlockMaker/gui/main_window.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QMainWindow
+from PyQt6.QtWidgets import QMainWindow, QFileDialog
 from .gui import Ui_MainWindow
 from .. import utils
 from ..peptide import Peptide
@@ -10,7 +10,7 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self) 
 
-        # Connect button click signals
+        # Connect button click signals to functions
         self.ui.toolButton_openfile.clicked.connect(self.open_text_file)
         self.ui.pushButton_addsequence.clicked.connect(self.add_sequence)
         self.ui.pushButton_deletesequence.clicked.connect(self.delete_sequence)
@@ -19,26 +19,30 @@ class MainWindow(QMainWindow):
 
     # Functions for buttons
     def open_text_file(self):
-        print("Open a text file.")
+        '''
+        Open a file dialog for selecting a text file with peptide sequences.
+        Display the location of the file in the UI.
+        Read the peptide sequences line by line, check their validity and add them to the list.
+        '''
+        file_path, _ = QFileDialog.getOpenFileName(None, "Select a Text File", "", "Text Files (*.txt);;All Files (*)")
+        self.ui.listWidget_filelocation.clear()
+        self.ui.listWidget_filelocation.addItem(file_path)
+        with open(file_path, "r") as file:
+            for line in file:
+                # TODO Check sequence validity
+                # Add to sequence list
+                self.ui.listWidget_sequences.addItem(line)
+
 
     def add_sequence(self):
         print("Add a sequence.")
 
     def delete_sequence(self):
-        print("Delete a sequence")
+        print("Delete selected sequence")
 
     def open_output_dir(self):
-        print("Select output directory.")
+        print("Select an output directory.")
 
     def generate_blocks(self):
-        print("Generate files.")
+        print("Generate block files.")
 
-
-# lineEdit_file
-# lineEdit_sequence
-# listWidget_sequences
-# comboBox_C_treatment
-# radioButton_M_oxidation
-# checkBox_<amino acid>
-# lineEdit_outputdir
-# pushButton_generateblocks
