6c9633bd0f7b168db148c164d2d9465b6f522a65
Steinar Gijze <steinar-gijze@hotmail.nl>
2025-02-06 13:11:57 +0100

    Map buttons to functions

--- BlockMaker.py
+++ BlockMaker.py
@@ -1,12 +1,10 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QStyleFactory
 from BlockMaker.gui.main_window import MainWindow
-from BlockMaker import utils
-from BlockMaker.peptide import Peptide
-
 
 
 def main():
+    # Create instance of QApplication
     app = QApplication(sys.argv)
 
     # Set the application style to Fusion
@@ -16,26 +14,8 @@ def main():
     main_window = MainWindow()
     main_window.show()
 
+    # Start the application's event loop and exit the program when the loop ends
     sys.exit(app.exec())
-
-
-    # # Ask for user input and generate block file.
-    # while True:
-    #     sequence = utils.get_input_sequence()
-    #     name = utils.get_block_name()
-    #     peptide = Peptide(sequence, name)
-    #     peptide.write_block_file()  
-
-    #     # Ask user if they want to create another block file or quit
-    #     while True:
-    #         input_continue = input("\nDo you want to create another block file? [Y/N]: ").strip().upper()
-    #         if input_continue == "Y":
-    #             break  # Exit the inner while-loop
-    #         elif input_continue == "N":
-    #             print("\nExiting the program...")
-    #             return  # Exit the main function
-    #         else:
-    #             print("\nInvalid input. Please enter 'Y' (yes) or 'N' (no).")
 
 
 
--- BlockMaker/gui/main_window.py
+++ BlockMaker/gui/main_window.py
@@ -1,5 +1,7 @@
 from PyQt6.QtWidgets import QMainWindow
 from .gui import Ui_MainWindow
+from .. import utils
+from ..peptide import Peptide
 
 
 class MainWindow(QMainWindow):
@@ -8,5 +10,35 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self) 
 
+        # Connect button click signals
+        self.ui.toolButton_openfile.clicked.connect(self.open_text_file)
+        self.ui.pushButton_addsequence.clicked.connect(self.add_sequence)
+        self.ui.pushButton_deletesequence.clicked.connect(self.delete_sequence)
+        self.ui.toolButton_openoutputdir.clicked.connect(self.open_output_dir)
+        self.ui.pushButton_generateblocks.clicked.connect(self.generate_blocks)
+
+    # Functions for buttons
+    def open_text_file(self):
+        print("Open a text file.")
+
+    def add_sequence(self):
+        print("Add a sequence.")
+
+    def delete_sequence(self):
+        print("Delete a sequence")
+
+    def open_output_dir(self):
+        print("Select output directory.")
+
+    def generate_blocks(self):
+        print("Generate files.")
 
 
+# lineEdit_file
+# lineEdit_sequence
+# listWidget_sequences
+# comboBox_C_treatment
+# radioButton_M_oxidation
+# checkBox_<amino acid>
+# lineEdit_outputdir
+# pushButton_generateblocks
