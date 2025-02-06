624692b2750e51ba7f84050b14fec32ca6a1c931
Steinar Gijze <steinar-gijze@hotmail.nl>
2025-02-06 10:10:03 +0100

    Launch UI from main file

--- BlockMaker.py
+++ BlockMaker.py
@@ -1,25 +1,41 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QStyleFactory
+from BlockMaker.gui.main_window import MainWindow
 from BlockMaker import utils
 from BlockMaker.peptide import Peptide
 
 
-def main():
-    # Ask for user input and generate block file.
-    while True:
-        sequence = utils.get_input_sequence()
-        name = utils.get_block_name()
-        peptide = Peptide(sequence, name)
-        peptide.write_block_file()  
 
-        # Ask user if they want to create another block file or quit
-        while True:
-            input_continue = input("\nDo you want to create another block file? [Y/N]: ").strip().upper()
-            if input_continue == "Y":
-                break  # Exit the inner while-loop
-            elif input_continue == "N":
-                print("\nExiting the program...")
-                return  # Exit the main function
-            else:
-                print("\nInvalid input. Please enter 'Y' (yes) or 'N' (no).")
+def main():
+    app = QApplication(sys.argv)
+
+    # Set the application style to Fusion
+    app.setStyle(QStyleFactory.create("Fusion"))
+
+    # Create and show the main window
+    main_window = MainWindow()
+    main_window.show()
+
+    sys.exit(app.exec())
+
+
+    # # Ask for user input and generate block file.
+    # while True:
+    #     sequence = utils.get_input_sequence()
+    #     name = utils.get_block_name()
+    #     peptide = Peptide(sequence, name)
+    #     peptide.write_block_file()  
+
+    #     # Ask user if they want to create another block file or quit
+    #     while True:
+    #         input_continue = input("\nDo you want to create another block file? [Y/N]: ").strip().upper()
+    #         if input_continue == "Y":
+    #             break  # Exit the inner while-loop
+    #         elif input_continue == "N":
+    #             print("\nExiting the program...")
+    #             return  # Exit the main function
+    #         else:
+    #             print("\nInvalid input. Please enter 'Y' (yes) or 'N' (no).")
 
 
 
---
+++ BlockMaker/gui/main_window.py
@@ -0,0 +1,12 @@
+from PyQt6.QtWidgets import QMainWindow
+from .gui import Ui_MainWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self) 
+
+
+
