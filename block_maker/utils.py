f523ceab053ed49732c9a7ba8dcdfee1d393b1b4
Steinar Gijze <steinar-gijze@hotmail.nl>
2025-02-10 15:14:39 +0100

    Restore check_sequence_validity function

--- block_maker/utils.py
+++ block_maker/utils.py
@@ -1,5 +1,6 @@
 import datetime
 
+
 def write_to_log(message):
     '''Write message to log file with date and time'''
     with open("BlockMaker.log", "a") as file:  # Use "a" to append to the file
@@ -10,3 +11,17 @@ def write_to_log(message):
         else:
             file.write(f"\n{timestamp}\t{message}")
 
+
+def check_sequence_validity(sequence_input):
+    '''
+    Check the validity of an input peptide sequence.
+    Returns a dictionary with two lists: invalid characters and their positions
+    '''
+    invalid = {"positions": [], "characters": []}
+    # Loop over each character in the input sequence and look for invalid entries
+    for i, char in enumerate(sequence_input):
+        if char not in amino_acids.compositions.keys():
+            invalid["positions"].append(i + 1)  # Add index plus 1, so counting starts at 1
+            invalid["characters"].append(char)
+    # Return the dictionary
+    return invalid
